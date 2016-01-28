dd6fb23d8b44c668431d8d716c9f3312cfd9d823
황승리 <seungri-hwang@traport.com>
2016-01-28 15:25:00 +0900

    1. 필요없는 부분 삭제 2. Config 파일을 따로 빼놓음

---
+++ app/config/database.py
@@ -0,0 +1,7 @@
+#DB 설정파일
+database = {
+	'host':'devdb.travelhow.com',
+	'user':'travelhow',
+	'password':'port8812!!#',
+	'db':'TFCarMgr'
+}
---
+++ app/config/route.py
@@ -0,0 +1,35 @@
+#URL 맵핑 라우터
+routes = {
+        'members' : {
+            'memberInsert',
+            'memberGet',
+            'memberList',
+            'memberLogin'
+        },
+        'accounts' : {
+            'statusAccountInsert',
+            'statusAccountList'
+        },
+        'vehicles' : {
+            'vehicleInsert',
+            'vehicleCheckNumber',
+            'vehicleDelete'
+        },
+        'statusTypes' : {
+            'statusTypeList'
+        },
+        'statusSorts' : {
+            'statusSortList'
+        },
+        'memberEfficiencies' : {
+            'memberEfficiencyInsert',
+            'memberEfficiencyGet'
+        },
+        'fuelEfficiencies' : {
+            'fuelEfficiencyList'
+        },
+        'statusMileages' : {
+            'statusMileageList',
+            'statusMileageInsert'
+        }
+}
